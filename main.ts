c64fe233d532f369ef8131b39d4cbfec12b903e6
HanTJ <41634614+HanTJ@users.noreply.github.com>
2022-04-09 22:55:46 +0900

    알콜 센스 추가

--- main.ts
+++ main.ts
@@ -7,11 +7,21 @@ lcd1602.putString(
 3,
 0
 )
+lcd1602.putString(
+"ALC",
+7,
+0
+)
 let val = 0
 basic.forever(function () {
     lcd1602.putNumber(
-    pins.digitalReadPin(DigitalPin.P1),
-    7,
+    pins.analogReadPin(AnalogPin.P1),
+    4,
+    1
+    )
+    lcd1602.putNumber(
+    pins.analogReadPin(AnalogPin.P2),
+    9,
     1
     )
     basic.pause(1000)
